8a900c2a6fed670c3aa142c4df14c5bf35686f56
Kadir Akipek <akipekkadir23@gmail.com>
2025-05-25 11:56:03 +0300

    Add project files.

---
+++ CsharpEgitimKampi601/Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi601.Entities
+{
+	public class Customer
+	{
+        //MongoDb'de Id'ler string olarak tutulur
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CustomerId { get; set; } //attribute'lar ile CustomerId'yi benzersiz bir alana dönüştürdük
+        public string CustomerName { get; set; }
+        public string CustomerSurname { get; set; }
+        public string CustomerCity { get; set; }
+        public decimal CustomerBalance { get; set; }
+        public int CustomerShoppingCount { get; set; }
+    }
+}
---
+++ CsharpEgitimKampi601/Services/MongoDbConnection.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpEgitimKampi601.Services
+{
+	public class MongoDbConnection
+	{
+		private IMongoDatabase _database;
+        public MongoDbConnection()
+        {
+            var client = new MongoClient("mongodb://localhost:27017"); //veritabanı bağlantı adresimiz
+            _database = client.GetDatabase("Db601Customer"); //veriTabanımızı oluşturduk
+        }
+        public IMongoCollection<BsonDocument> GetCustomersCollection() //koleksiyon oluşturma(çağırma) metodumuz
+        {
+            return _database.GetCollection<BsonDocument>("Customers"); //Db601 veritbanımızın içine kolkesiyon ekledik
+        }
+    }
+}
